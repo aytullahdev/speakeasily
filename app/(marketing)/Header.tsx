d775eee072c4bb185b98ecb566266cfc444fdc63
ayat-liberatelabs <ayat@liberate-labs.com>
2024-06-30 21:14:38 +0600

    feat: layout added

---
+++ app/(marketing)/Header.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <header className="h-20 w-full border-b-2 border-slate-300 px-4"></header>
+  );
+};
--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button";
 
 export default async function Home() {
-  return <div className="text-green-500"></div>;
+  return <div className="text-green-500">Hi</div>;
 }
