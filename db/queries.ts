27316b12165b741d4a6a7f0e5c812b43424706e3
ayat-liberatelabs <ayat@liberate-labs.com>
2024-07-03 02:40:12 +0600

    fix: user id change to string

--- db/queries.ts
+++ db/queries.ts
@@ -1,7 +1,25 @@
 import { cache } from "react";
 import db from "./drizzle";
-
+import { auth } from "@clerk/nextjs/server";
+import { userProgress } from "./schema";
+import { eq } from "drizzle-orm";
 export const getCourses = cache(async () => {
   const data = await db.query.courses.findMany();
+  return data;
+});
+
+export const getUserProgress = cache(async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return null;
+  }
+  console.log("userId", userId);
+  const data = await db.query.userProgress.findFirst({
+    where: eq(userProgress.userId, userId),
+    with: {
+      activeCourse: true,
+    },
+  });
   return data;
 });
