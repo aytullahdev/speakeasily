a1c86cd6963000324154ec00021689cf1049dea0
ayat-liberatelabs <ayat@liberate-labs.com>
2024-07-06 01:57:49 +0600

    feat: challenge card added

--- app/lesson/Quiz.tsx
+++ app/lesson/Quiz.tsx
@@ -4,6 +4,8 @@ import { challengeOptions, challenges } from "@/db/schema";
 
 import { useState } from "react";
 import { Header } from "./Header";
+import { QuestionBubble } from "./QuestionBubble";
+import { Challenge } from "./Challenge";
 
 type Props = {
   initialPercentage: number;
@@ -25,6 +27,19 @@ export const Quiz = ({
 }: Props) => {
   const [hearts, setHearts] = useState(initialHearts);
   const [percentage, setPercentage] = useState(initialPercentage);
+  const [challenges] = useState(initialLessonChallenges);
+  const [activeIndex, setActiveIndex] = useState(() => {
+    const unCompletedIndex = challenges.findIndex(
+      (challenge) => !challenge.completed
+    );
+    return unCompletedIndex === -1 ? 0 : unCompletedIndex;
+  });
+  const challenge = challenges[activeIndex];
+  const options = challenge?.challengeOptions || [];
+  const title =
+    challenge.type === "ASSIST"
+      ? "Select the correct meaning"
+      : challenge.question;
   return (
     <>
       <Header
@@ -32,7 +47,28 @@ export const Quiz = ({
         percentage={percentage}
         hasActiveSubscription={userSubscription?.active}
       />
-      <h1>Quiz</h1>
+      <div className="flex-1">
+        <div className="h-full flex items-center justify-center">
+          <div className="lg:min-h-[350px] lg:w-[600px[ w-full px-6 lg:px-0 flex flex-col gap-y-12">
+            <h1 className="text-lg lg:text-3xl text-center lg:text-start font-bold text-neutral-700">
+              {title}
+            </h1>
+            <div>
+              {challenge?.type === "ASSIST" && (
+                <QuestionBubble question={challenge.question} />
+              )}
+              <Challenge
+                options={options}
+                onSelect={() => {}}
+                status="wrong"
+                selectedOption={undefined}
+                disabled={false}
+                type={challenge.type}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
